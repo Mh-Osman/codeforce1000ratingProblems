a695d8f4a4365f546adea7117b42eaf2b4295de4
Mh-Osman <osmangani1osm@gmail.com>
2024-10-05 18:18:59 +0600

    1659 A. Red Versus Blue

---
+++ A_Red_Versus_Blue.cpp
@@ -0,0 +1,55 @@
+//My name is Osman Gani
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+   
+    int t;
+    cin>>t;
+    while(t--){
+
+    int n ,r , b;
+    cin>>n>>r>>b;
+      b=b+1;
+    vector<string>v(b, "B");
+    v[0]="";
+  
+     while(b!=0){
+         
+         int s = r/(b);
+         string str(s , 'R');
+         for(int i=0;i<b;i++){
+
+            v[i]+=str;
+         }
+         if(r%b == 0){
+            b=0;
+            break;
+         }else{
+
+            r=r%b;
+            b=r;
+         }
+
+
+
+
+     }
+
+ for(auto x:v){
+
+    cout<<x;
+ }
+ cout<<endl;
+
+
+
+    } 
+       
+   
+    
+
+     
+    return 0;
+}
